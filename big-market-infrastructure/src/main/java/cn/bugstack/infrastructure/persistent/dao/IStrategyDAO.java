e77792eda7e091d901dc3a863d011b8caa353524
DDDDW-JB <wang1660472963@163.com>
2024-07-29 00:09:56 +0800

    2024-7-29:完成基础层orm

---
+++ big-market-infrastructure/src/main/java/cn/bugstack/infrastructure/persistent/dao/IStrategyDAO.java
@@ -0,0 +1,13 @@
+package cn.bugstack.infrastructure.persistent.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * ClassName: IStrategyDAO
+ * Description:
+ *
+ * @Create 2024/7/28 23:10
+ */
+@Mapper
+public interface IStrategyDAO {
+}
